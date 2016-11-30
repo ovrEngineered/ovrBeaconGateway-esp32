21902e054724d24d353f2c9c9a65aab216654392
Christopher Armenio <christopher.armenio@ovrengineered.com>
2016-11-29 23:08:05 -0500

    Cleaned up main.c and started transitioning code into wifiManager

--- project/src/main.c
+++ project/src/main.c
@@ -1,43 +1,105 @@
+/**
+ * @file
+ * @copyright 2016 opencxa.org
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * @author Christopher Armenio
+ */
+
+
+// ******** includes ********
 #include "freertos/FreeRTOS.h"
-#include "esp_wifi.h"
+#include "freertos/task.h"
+
 #include "esp_system.h"
-#include "esp_event.h"
-#include "esp_event_loop.h"
 #include "nvs_flash.h"
-#include "driver/gpio.h"
 
-esp_err_t event_handler(void *ctx, system_event_t *event)
-{
-    return ESP_OK;
-}
+#include <cxa_assert.h>
+#include <cxa_console.h>
+#include <cxa_delay.h>
+#include <cxa_esp32_gpio.h>
+#include <cxa_esp32_timeBase.h>
+#include <cxa_esp32_usart.h>
+#include <cxa_network_wifiManager.h>
+#include <cxa_runLoop.h>
 
+#define CXA_LOG_LEVEL			CXA_LOG_LEVEL_TRACE
+#include <cxa_logger_implementation.h>
+
+
+// ******** local macro definitions ********
+
+
+// ******** local type definitions *******
+
+
+// ******** local function prototypes ********
+static void sysInit();
+static void userTask(void *pvParameters);
+
+
+// ******** local variable declarations ********
+static cxa_esp32_gpio_t gpio_led;
+static cxa_esp32_usart_t usart_debug;
+
+
+// ******** global function implementations ********
 void app_main(void)
 {
     nvs_flash_init();
-    system_init();
-    tcpip_adapter_init();
-    ESP_ERROR_CHECK( esp_event_loop_init(event_handler, NULL) );
-    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
-    ESP_ERROR_CHECK( esp_wifi_init(&cfg) );
-    ESP_ERROR_CHECK( esp_wifi_set_storage(WIFI_STORAGE_RAM) );
-    ESP_ERROR_CHECK( esp_wifi_set_mode(WIFI_MODE_STA) );
-    wifi_config_t sta_config = {
-        .sta = {
-            .ssid = "access_point_name",
-            .password = "password",
-            .bssid_set = false
-        }
-    };
-    ESP_ERROR_CHECK( esp_wifi_set_config(WIFI_IF_STA, &sta_config) );
-    ESP_ERROR_CHECK( esp_wifi_start() );
-    ESP_ERROR_CHECK( esp_wifi_connect() );
 
-    gpio_set_direction(GPIO_NUM_4, GPIO_MODE_OUTPUT);
-    int level = 0;
-    while (true) {
-        gpio_set_level(GPIO_NUM_4, level);
-        level = !level;
-        vTaskDelay(300 / portTICK_PERIOD_MS);
-    }
+
+//    ESP_ERROR_CHECK( esp_wifi_set_mode(WIFI_MODE_STA) );
+//    wifi_config_t sta_config = {
+//        .sta = {
+//            .ssid = "access_point_name",
+//            .password = "password",
+//            .bssid_set = false
+//        }
+//    };
+//    ESP_ERROR_CHECK( esp_wifi_set_config(WIFI_IF_STA, &sta_config) );
+//    ESP_ERROR_CHECK( esp_wifi_start() );
+//    ESP_ERROR_CHECK( esp_wifi_connect() );
+
+    sysInit();
 }
 
+
+// ******** local function implementations ********
+static void sysInit()
+{
+	// setup our debug serial console
+	cxa_esp32_usart_init_noHH(&usart_debug, UART_NUM_0, 115200);
+	cxa_assert_setIoStream(cxa_usart_getIoStream(&usart_debug.super));
+
+	// setup our timebase
+	cxa_esp32_timeBase_init();
+
+	// setup our console and logger
+	cxa_console_init("Haven ECB", cxa_usart_getIoStream(&usart_debug.super));
+	cxa_logger_setGlobalIoStream(cxa_usart_getIoStream(&usart_debug.super));
+
+	// setup our application-specific peripherals
+	cxa_network_wifiManager_init();
+	cxa_esp32_gpio_init_output(&gpio_led, GPIO_NUM_4, CXA_GPIO_POLARITY_NONINVERTED, 0);
+
+	xTaskCreate(userTask, (const char * const)"usrTask", 2048, NULL, tskIDLE_PRIORITY, NULL);
+}
+
+
+static void userTask(void *pvParameters)
+{
+	cxa_network_wifiManager_start();
+
+	// does not return
+	cxa_runLoop_execute();
+}
