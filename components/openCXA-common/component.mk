3d88ce1d2d956ca5897f0d953b16778a65c46094
Christopher Armenio <christopher.armenio@ovrengineered.com>
2016-12-08 18:21:39 -0500

    Working commit for AWS IoT demo

--- components/openCXA-common/component.mk
+++ components/openCXA-common/component.mk
@@ -23,7 +23,9 @@ COMPONENT_ADD_INCLUDEDIRS := \
 		$(OPENCXA_ROOT)/include/misc \
 		$(OPENCXA_ROOT)/include/mqtt \
 		$(OPENCXA_ROOT)/include/mqtt/messages \
+		$(OPENCXA_ROOT)/include/mqtt/rpc \
 		$(OPENCXA_ROOT)/include/net \
+		$(OPENCXA_ROOT)/include/net/lwipMbedTls \
 		$(OPENCXA_ROOT)/include/peripherals \
 		$(OPENCXA_ROOT)/include/runLoop \
 		$(OPENCXA_ROOT)/include/serial \
@@ -39,7 +41,9 @@ COMPONENT_SRCDIRS := \
 		$(OPENCXA_ROOT)/src/misc \
 		$(OPENCXA_ROOT)/src/mqtt \
 		$(OPENCXA_ROOT)/src/mqtt/messages \
+		$(OPENCXA_ROOT)/src/mqtt/rpc \
 		$(OPENCXA_ROOT)/src/net \
+		$(OPENCXA_ROOT)/src/net/lwipMbedTls \
 		$(OPENCXA_ROOT)/src/peripherals \
 		$(OPENCXA_ROOT)/src/runLoop \
 		$(OPENCXA_ROOT)/src/serial \
--- project/src/main.c
+++ project/src/main.c
@@ -31,12 +31,19 @@
 #include <cxa_esp32_usart.h>
 #include <cxa_network_wifiManager.h>
 #include <cxa_runLoop.h>
+#include <cxa_uniqueId.h>
 
 #define CXA_LOG_LEVEL			CXA_LOG_LEVEL_TRACE
 #include <cxa_logger_implementation.h>
 
+#include "aws_certs.h"
+#include <cxa_mqtt_connectionManager.h>
+#include <cxa_mqtt_rpc_node_root.h>
+
 
 // ******** local macro definitions ********
+#define MQTT_SERVER 		"A218ORPUJ66O9J.iot.us-east-1.amazonaws.com"
+#define MQTT_PORT 			8883
 
 
 // ******** local type definitions *******
@@ -46,29 +53,24 @@
 static void sysInit();
 static void userTask(void *pvParameters);
 
+static cxa_mqtt_rpc_methodRetVal_t mqttRpcCb_onSetLed(cxa_mqtt_rpc_node_t *const nodeIn,
+		cxa_linkedField_t *const paramsIn, cxa_linkedField_t *const returnParamsOut,
+		void* userVarIn);
+
+static void consoleCb_rpcNotify(cxa_ioStream_t *const ioStreamIn, void* userVarIn);
+
 
 // ******** local variable declarations ********
 static cxa_esp32_gpio_t gpio_led;
 static cxa_esp32_usart_t usart_debug;
+
+static cxa_mqtt_rpc_node_root_t rpcNode_root;
 
 
 // ******** global function implementations ********
 void app_main(void)
 {
     nvs_flash_init();
-
-
-//    ESP_ERROR_CHECK( esp_wifi_set_mode(WIFI_MODE_STA) );
-//    wifi_config_t sta_config = {
-//        .sta = {
-//            .ssid = "access_point_name",
-//            .password = "password",
-//            .bssid_set = false
-//        }
-//    };
-//    ESP_ERROR_CHECK( esp_wifi_set_config(WIFI_IF_STA, &sta_config) );
-//    ESP_ERROR_CHECK( esp_wifi_start() );
-//    ESP_ERROR_CHECK( esp_wifi_connect() );
 
     sysInit();
 }
@@ -85,21 +87,56 @@ static void sysInit()
 	cxa_esp32_timeBase_init();
 
 	// setup our console and logger
-	cxa_console_init("Haven ECB", cxa_usart_getIoStream(&usart_debug.super));
+	cxa_console_init("AWS IoT Tester", cxa_usart_getIoStream(&usart_debug.super));
 	cxa_logger_setGlobalIoStream(cxa_usart_getIoStream(&usart_debug.super));
 
 	// setup our application-specific peripherals
 	cxa_network_wifiManager_init();
 	cxa_esp32_gpio_init_output(&gpio_led, GPIO_NUM_4, CXA_GPIO_POLARITY_NONINVERTED, 0);
 
-	xTaskCreate(userTask, (const char * const)"usrTask", 2048, NULL, tskIDLE_PRIORITY, NULL);
+	cxa_mqtt_connManager_init_clientCert(NULL,
+										 MQTT_SERVER, MQTT_PORT,
+										 tls_server_root_cert, sizeof(tls_server_root_cert),
+										 tls_client_cert, sizeof(tls_client_cert),
+										 tls_client_key_private, sizeof(tls_client_key_private));
+	cxa_mqtt_rpc_node_root_init(&rpcNode_root, cxa_mqtt_connManager_getMqttClient(), true, cxa_uniqueId_getHexString());
+
+	// XXX/->setLed/0000
+	cxa_mqtt_rpc_node_addMethod(&rpcNode_root.super, "setLed", mqttRpcCb_onSetLed, NULL);
+	cxa_console_addCommand("rpc_notify", consoleCb_rpcNotify, NULL);
+
+	// schedule our user task for execution
+	xTaskCreate(userTask, (const char * const)"usrTask", 4096, NULL, tskIDLE_PRIORITY, NULL);
 }
 
 
 static void userTask(void *pvParameters)
 {
-	cxa_network_wifiManager_start();
+	cxa_network_wifiManager_startNormal();
 
 	// does not return
 	cxa_runLoop_execute();
+}
+
+
+static cxa_mqtt_rpc_methodRetVal_t mqttRpcCb_onSetLed(cxa_mqtt_rpc_node_t *const nodeIn,
+		cxa_linkedField_t *const paramsIn, cxa_linkedField_t *const returnParamsOut,
+		void* userVarIn)
+{
+	if( cxa_linkedField_getSize_bytes(paramsIn) != 1 ) return CXA_MQTT_RPC_METHODRETVAL_FAIL_INVALIDPARAMS;
+
+	uint8_t ledVal = 0;
+	cxa_linkedField_get_uint8(paramsIn, 0, ledVal);
+	if( ledVal == '0' ) ledVal = 0;
+
+	cxa_gpio_setValue(&gpio_led.super, ledVal);
+
+	return CXA_MQTT_RPC_METHODRETVAL_SUCCESS;
+}
+
+
+static void consoleCb_rpcNotify(cxa_ioStream_t *const ioStreamIn, void* userVarIn)
+{
+	uint8_t ledState = cxa_gpio_getValue(&gpio_led.super);
+	cxa_mqtt_rpc_node_publishNotification(&rpcNode_root.super, "ledStatus", CXA_MQTT_QOS_ATMOST_ONCE, &ledState, sizeof(ledState));
 }
